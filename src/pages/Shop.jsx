d8adea983405eaf6f6eb611180c2c429a70da001
Saurav Sharma <sauravsharma691999@gmail.com>
2024-06-30 05:41:17 -0400

    add

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Component/hero/hero'
+import Popular from '../Component/Popular/Popular'
 
 const shop = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
     </div>
   )
 }
